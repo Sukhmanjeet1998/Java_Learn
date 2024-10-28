d0cd8234ad8220cf6ccac2d41a6e46c477ac500d
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-10-28 14:52:32 +0000

    Java

---
+++ CoreJava.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+/**
+ * CoreJava
+ */
+public class CoreJava {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int num1 = 6;   // literals
+        byte b = 127;
+        short sh = 558;
+        long ln = 5854l;
+        float f = 5.5f;
+        double d = 5.7;
+        char ch = 'a';
+        char ca = 97;
+        boolean bool = true;
+
+        System.out.println(ln);
+        System.out.println(ca);
+        
+    }
+}
